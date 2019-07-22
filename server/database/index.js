e27395484fb5c0b0e9da3e701dab1bb356f299ce
Benson Leong <benson@Bensons-MacBook-Pro.local>
2019-07-22 19:42:15 -0400

    Created Profile model and added to database/index.js; still need to include state abbrevs

--- server/database/index.js
+++ server/database/index.js
@@ -1,5 +1,10 @@
 const db = require('./db');
 //import models here
 
+const Profile = require("./models/Profile");
+
 // export models here
-module.exports = { db };
+module.exports = { 
+    db,
+    Profile
+ };
--- server/database/models/Profile.js
+++ server/database/models/Profile.js
@@ -2,7 +2,37 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const Profile = db.define('profile', {
-    
+    first_name : {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    last_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    street_address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    state: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isIn: [/* abbrevs for all 50 states */]
+        }
+    },
+    zipcode: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    phone_number: {
+        type: Sequelize.INTEGER,
+        defaultValue: 424-242-4242
+    }
 });
 
 module.exports = Profile;
