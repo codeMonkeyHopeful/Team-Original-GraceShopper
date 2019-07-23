c27653befe6c161ece99b33178df529e1f9a3f85
Benson Leong <benson@Bensons-MacBook-Pro.local>
2019-07-23 18:57:35 -0400

    Add some data to seed

--- server/database/utils/seed.js
+++ server/database/utils/seed.js
@@ -1,1 +1,35 @@
 // seed shit here
+const { db, Profile, Product, Review, User } = require('../index.js');
+
+const users = [
+  {
+    email: 'benson@fsaogs.com',
+    password: 'fsanyc',
+    isAdmin: true,
+  },
+  {
+    email: 'mark@fsaogs.com',
+    password: 'fsanyc',
+    isAdmin: true,
+  },
+  {
+    email: 'ryan@fsaogs.com',
+    password: 'fsanyc',
+    isAdmin: true,
+  },
+  {
+    email: 'russell@fsa.com',
+    password: 'fsata',
+    isAdmin: false,
+  },
+];
+
+const product = [
+  {
+    sku: 'longtext',
+    name: 'AA Batteries',
+    category: 'Accessories',
+    description: 'Give your electronics some juice, bro',
+    price: 3.99,
+  },
+];
