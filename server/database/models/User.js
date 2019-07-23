0b54ccca07a8055e259500f274948214a67f0ab9
HypeDis <m.bae@live.com>
2019-07-23 16:44:33 -0400

    removed username field

--- server/database/models/User.js
+++ server/database/models/User.js
@@ -2,13 +2,6 @@ const Sequelize = require('sequelize');
 const db = require('./../db');
 
 const User = db.define('user', {
-  username: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
   email: {
     type: Sequelize.STRING,
     allowNull: false,
