04fdddaaee9d4b6e8746d94969582277877d4e44
Ryan Jasinski <jasinski.17@gmail.com>
2019-07-22 20:26:14 -0400

    Addition of Product and Review Models

---
+++ server/database/models/Product.js
@@ -0,0 +1,81 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Product = db.define("product", {
+  sku: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  name: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  product_line_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  category: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    },
+    parent_category_1: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    parent_category_2: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    parent_category_3: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    }
+  },
+  price: {
+    type: Sequelize.NUMBER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  image_url: {
+    type: Sequelize.STRING,
+    defaultValue: "https://picsum.photos/200" //will generate a random image on ever refresh of the page
+  },
+  brand_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+});
+
+module.exports = Product;
---
+++ server/database/models/Review.js
@@ -0,0 +1,21 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Review = db.define("review", {
+  rating: {
+    type: Sequelize.NUMBER,
+    allowNull: false,
+    validate: {
+      notEmty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+});
+
+module.exports = Review;
