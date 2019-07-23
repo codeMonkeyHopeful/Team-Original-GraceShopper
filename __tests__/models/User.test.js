edefcbcd2a5eab39f974ffcfdf6762fb0e9a51c7
HypeDis <m.bae@live.com>
2019-07-23 16:43:43 -0400

    user test skeleton

--- __tests__/models/User.test.js
+++ __tests__/models/User.test.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 require('dotenv').config();
-
->>>>>>> brought in stash from another branch
 const { db, User } = require('./../../server/database');
+
 beforeAll(async () => {
   await db.sync({ force: true });
 });
@@ -11,27 +8,16 @@ afterAll(async () => {
   await db.close();
 });
 
-describe('adding a user', async () => {
-<<<<<<< HEAD
-  const user = User.create({
-    username: 'user123',
-    email: 'user123@gmail.com',
+describe('adding a user', () => {
+  const userObj = {
+    email: 'james@gmail.com',
     password: 'password',
-  });
-  test('it should create a user when all fields are valid', () => {
-    expect(user.username).toEqual('user123');
-=======
+  };
   test('it should create a user when all fields are valid', async () => {
-    try {
-      const user = await User.create({
-        username: 'user123',
-        email: 'james@gmail.com',
-        password: 'password',
-      });
-      expect(user.username).toEqual('user123');
-    } catch (e) {
-      console.error('user test error', e);
-    }
->>>>>>> brought in stash from another branch
+    const user = await User.create(userObj);
+    expect(user.email).toEqual('james@gmail.com');
+    expect(user.password).toBeDefined();
   });
+  test.todo('should not allow duplicate email');
+  test.todo('pw should be hashed correctly'); // possibly use a snapshot here
 });
