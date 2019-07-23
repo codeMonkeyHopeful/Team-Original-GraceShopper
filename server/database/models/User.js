ed6ef7f205f62619e8370f7f15938c77ecb08092
HypeDis <m.bae@live.com>
2019-07-22 20:18:35 -0400

    basic user model

--- server/database/models/User.js
+++ server/database/models/User.js
@@ -1,3 +1,39 @@
+<<<<<<< HEAD
 //make a model
 
 const User = db.define('Profile');
+=======
+const Sequelize = require('sequelize');
+const db = require('./../db');
+
+const User = db.define('user', {
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+// add password hashing hook
+>>>>>>> basic user model
