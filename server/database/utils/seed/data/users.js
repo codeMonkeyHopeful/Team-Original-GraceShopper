dfec2acc4054d9fa2f05676f2f9803f5c09b5b4f
hypedis <m.bae@live.com>
2019-07-23 23:42:17 -0400

    modularized seed data and created a user creator function

---
+++ server/database/utils/seed/data/users.js
@@ -0,0 +1,25 @@
+const faker = require('faker');
+const users = [
+  {
+    email: 'benson@fsaogs.com',
+    password: 'fsanyc',
+    isAdmin: true,
+  },
+  {
+    email: 'mark@fsaogs.com',
+    password: 'fsanyc',
+    isAdmin: true,
+  },
+  {
+    email: 'ryan@fsaogs.com',
+    password: 'fsanyc',
+    isAdmin: true,
+  },
+  {
+    email: 'russell@fsa.com',
+    password: 'fsata',
+    isAdmin: false,
+  },
+];
+
+module.exports = users;
