f36265ce69fde58199adbe99764efca6d2d0fbe9
Ryan Jasinski <jasinski.17@gmail.com>
2019-07-24 20:24:02 -0400

    seed file created for products

---
+++ server/database/utils/seed/data/products.js
@@ -0,0 +1,84 @@
+const products = [
+  {
+    sku: '1123',
+    name: 'Intel CPU',
+    product_line_id: 1,
+    category: 'Computers',
+    product_category_1: 'Computers',
+    product_category_2: 'Parts',
+    product_category_3: 'CPU',
+    description: 'An Intel CPU, fresh off the presses',
+    price: 199.99,
+    image_url: 'https://picsum.photos/200',
+    brand_id: 1, //update
+  },
+  {
+    sku: '1234',
+    name: 'Asus',
+    product_line_id: 1,
+    category: 'Computers',
+    product_category_1: 'Computers',
+    product_category_2: 'Parts',
+    product_category_3: 'Motherboards',
+    description: 'Asus Motherboard for Intel CPU',
+    price: 59.99,
+    image_url: 'https://picsum.photos/200',
+    brand_id: 1, //update
+  },
+  {
+    sku: '2234',
+    name: 'Mechanical Typewriter',
+    product_line_id: 1,
+    category: 'Typerwriters',
+    product_category_1: 'Typerriters',
+    product_category_2: 'Mechanical',
+    product_category_3: 'Screen',
+    description:
+      'Brand new typewriter with a visual screen to view what you are typing, the future',
+    price: 599.99,
+    image_url: 'https://picsum.photos/200',
+    brand_id: 1, //update
+  },
+  {
+    sku: '2456',
+    name: 'Electronic Typewriter',
+    product_line_id: 1,
+    category: 'Typerwriters',
+    product_category_1: 'Typewriters',
+    product_category_2: 'Electronic',
+    product_category_3: 'No Screen',
+    description: 'A new and improved typewriter with all the fixins',
+    price: 1000.0,
+    image_url: 'https://picsum.photos/200',
+    brand_id: 1, //update
+  },
+  {
+    sku: '3456',
+    name: 'Handheld Calculator',
+    product_line_id: 1,
+    category: 'Data Devices',
+    product_category_1: 'Data Devices',
+    product_category_2: 'Hand Held',
+    product_category_3: 'Calculator',
+    description: 'TI-86 graphing calculator to meet all your mathing needs',
+    price: 99.99,
+    image_url: 'https://picsum.photos/200',
+    brand_id: 1, //update
+  },
+  {
+    sku: '3555',
+    name: 'Electronic Translator',
+    product_line_id: 1,
+    category: 'Data Devices',
+    product_category_1: 'Data Devices',
+    product_category_2: 'Hand Held',
+    product_category_3: 'Translator',
+    description:
+      'Translate up to 50 foreign languages in the palm of your hand!',
+    price: 59.99,
+    image_url: 'https://picsum.photos/200',
+    brand_id: 1, //update
+  },
+];
+
+module.exports = products;
