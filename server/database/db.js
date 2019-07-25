8c53171e64b2ecac367655473e33dc6b4d32cafe
hypedis <m.bae@live.com>
2019-07-25 00:31:31 -0400

    only require dotenv when not in production mode

--- server/database/db.js
+++ server/database/db.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const Sequelize = require('sequelize');
 const db = new Sequelize(process.env.DATABASE_URI, { logging: false });
 
