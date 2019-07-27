e740c1573a705580f262a3dbffb845a5cb49aad8
Ryan Jasinski <jasinski.17@gmail.com>
2019-07-27 13:50:24 -0400

    updated images for products

--- server/database/utils/seed/data/products.js
+++ server/database/utils/seed/data/products.js
@@ -8,7 +8,8 @@ const products = [
     parent_category_3: 112,
     description: 'An Intel CPU, fresh off the presses',
     price: 199.99,
-    image_url: 'https://picsum.photos/200',
+    image_url:
+      'https://images-na.ssl-images-amazon.com/images/I/51r5Y8XozrL._SX425_.jpg',
     brand_id: 1, //update
     productCategoryId: 2,
   },
@@ -21,7 +22,8 @@ const products = [
     parent_category_3: 111,
     description: 'Asus Motherboard for Intel CPU',
     price: 59.99,
-    image_url: 'https://picsum.photos/200',
+    image_url:
+      'https://media.karousell.com/media/photos/products/2018/04/22/old_motherboard_1524363280_5aeb172f.jpg',
     brand_id: 1, //update,
     productCategoryId: 1,
   },
@@ -35,7 +37,8 @@ const products = [
     description:
       'Brand new typewriter with a visual screen to view what you are typing, the future',
     price: 599.99,
-    image_url: 'https://picsum.photos/200',
+    image_url:
+      'https://cdn.shopify.com/s/files/1/0557/2745/products/QWS_Store_Black_1_1_1600x.png?v=1549488087',
     brand_id: 1, //update
     productCategoryId: 3,
   },
@@ -48,7 +51,8 @@ const products = [
     parent_category_3: 212,
     description: 'A new and improved typewriter with all the fixins',
     price: 1000.0,
-    image_url: 'https://picsum.photos/200',
+    image_url:
+      'https://technabob.com/blog/wp-content/uploads/2014/10/hemingwrite-by-adam-leeb-and-patrick-paul.jpg',
     brand_id: 1, //update
     productCategoryId: 4,
   },
@@ -61,7 +65,7 @@ const products = [
     parent_category_3: 311,
     description: 'TI-86 graphing calculator to meet all your mathing needs',
     price: 99.99,
-    image_url: 'https://picsum.photos/200',
+    image_url: 'http://www.vintagecalculators.com/assets/images/TI2500_1.jpg',
     brand_id: 1, //update
     productCategoryId: 5,
   },
@@ -75,7 +79,8 @@ const products = [
     description:
       'Translate up to 50 foreign languages in the palm of your hand!',
     price: 59.99,
-    image_url: 'https://picsum.photos/200',
+    image_url:
+      'https://www.picclickimg.com/d/l400/pict/223453646779_/Craig-M100-Handheld-Translator-English-French-German-Capsules.jpg',
     brand_id: 1, //update
     productCategoryId: 6,
   },
