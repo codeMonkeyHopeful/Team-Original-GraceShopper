d2f874ec5b5ab5ce7de516f16e2e2b06d1c8ee48
Ryan Jasinski <jasinski.17@gmail.com>
2019-07-29 18:49:46 -0400

    fixing econn error

--- server/database/db.js
+++ server/database/db.js
@@ -3,9 +3,6 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const Sequelize = require('sequelize');
-const db = new Sequelize(
-  process.env.DATABASE_URI || 'postgres://localhost:5432/grace_shopper',
-  { logging: false }
-);
+const db = new Sequelize(process.env.DATABASE_URI, { logging: false });
 
 module.exports = db;
