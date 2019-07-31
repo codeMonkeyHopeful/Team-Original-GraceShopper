8ccb1378c6e7b61048a9de2a9ff69686fc9da3af
Ryan Jasinski <jasinski.17@gmail.com>
2019-07-31 19:55:48 -0400

    added product reducer and added to root reducer

--- client/redux/reducers/productReducer.js
+++ client/redux/reducers/productReducer.js
@@ -1,7 +1,34 @@
+import {
+  GOT_ALL_PRODUCTS,
+  GOT_TOP_TIER_CATS,
+  INCREMENT_PROD_LEVEL,
+  GOT_UPDATED_CATS,
+} from '../actionCreators/productsCreators';
+import store from '..';
+
 const intialState = {
   productLevel: 1,
   allProducts: [], //will not change
-  topLevelProducts: [], //will not change
+  topLevelCategories: [], //will not change
   currentCategories: [],
   currentProducts: [],
 };
+
+const productReducer = (state = intialState, action) => {
+  switch (action.type) {
+    case GOT_ALL_PRODUCTS:
+      return { ...state, allProducts: action.products };
+    case GOT_TOP_TIER_CATS:
+      return { ...state, topLevelCategories: action.categories };
+    case INCREMENT_PROD_LEVEL:
+      return { ...state, productLevel: this.productLevel + 1 };
+    case GOT_UPDATED_CATS:
+      return { ...store, currentCategories: action.updatedCats };
+    case GOT_UPDATED_PRODUCTS:
+      return { ...store, currentProducts: action.updatedProducts };
+    default:
+      return state;
+  }
+};
+
+export default productReducer;
