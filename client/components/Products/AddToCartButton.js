9c84e80d98180bd59756ac1d15bdf4ffe445753d
Benson <ben72s5on@gmail.com>
2019-08-06 00:49:26 -0400

    merged dev to working branch

--- client/components/Products/AddToCartButton.js
+++ client/components/Products/AddToCartButton.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { addToCart } from '../../redux';
-
 const AddToCartButton = props => {
-  const { productId, qty, userId, price, cart, addToCart } = props;
+  const { productId, qty, userId } = props;
   const handleSubmit = e => {
     e.preventDefault();
     if (!productId) {
@@ -11,7 +9,6 @@ const AddToCartButton = props => {
       return;
     } else {
       // code for adding to cart goes here
-      addToCart(productId, qty, price);
       // userId will be undefined if not logged in. Check for that on the api route and use sessionId instead if not logged in
       // console.table({
       //   'userId ': userId,
@@ -27,19 +24,5 @@ const mapState = ({
   currentUser: {
     profile: { userId },
   },
-  product,
-  cart,
-}) => ({ userId, cart, product });
-
-const mapDispatch = dispatch => {
-  return {
-    addToCart: (pid, qty, price) => {
-      dispatch(addToCart(pid, qty, price));
-    },
-  };
-};
-
-export default connect(
-  mapState,
-  mapDispatch
-)(AddToCartButton);
+}) => ({ userId });
+export default connect(mapState)(AddToCartButton);
