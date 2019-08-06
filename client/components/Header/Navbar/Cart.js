f2a00b53ed4cff45091ae56f4601e367740e3b26
Benson <ben72s5on@gmail.com>
2019-08-06 14:28:16 -0400

    Cart currently only adds products from front end and does not persist in database

--- client/components/Header/Navbar/Cart.js
+++ client/components/Header/Navbar/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { CART_ITEMS_TITLE, CART_CONTAINER, CARTEGORIES } from '../styles';
+import { CART_ITEMS_TITLE, CART_CONTAINER } from '../styles';
+import CartTable from './CartTable';
 
 const Cart = props => {
   console.log('props from cart route', props);
@@ -11,32 +12,7 @@ const Cart = props => {
       <div id="cart-items-title" style={CART_ITEMS_TITLE}>
         Items
       </div>
-      <table id="cart-products">
-        <th id="cart-products-header">
-          <tr>PRODUCT</tr>
-          <tr>QUANTITY</tr>
-          <tr>PRICE</tr>
-        </th>
-        {cart.map(({ pid, qty, price }) => {
-          return (
-            <span>
-              <tr>{pid}</tr>
-              <tr>{qty}</tr>
-              <tr>{price}</tr>
-            </span>
-          );
-        })}
-      </table>
-
-      {/* <div id="products" style={CARTEGORIES}>
-        {cart.map(({ pid, qty, price }) => {
-          return (
-            <div id="cartegories">
-              {pid} {qty} {price}
-            </div>
-          );
-        })}
-      </div> */}
+      <CartTable />
     </div>
   );
 };
--- client/components/Header/styles.js
+++ client/components/Header/styles.js
@@ -58,17 +58,18 @@ export const NAV_IMAGE = {
 export const CART_CONTAINER = {
   disply: 'flex',
   justifyContent: 'center',
+  margin: '0 150px 0 150px',
 };
 
 export const CART_ITEMS_TITLE = {
   display: 'flex',
   border: '5px solid grey',
   padding: '2px',
-  margin: '0 200px 0 200px',
 };
 
-export const CARTEGORIES = {
-  display: 'flex',
-  marginLeft: '200px',
-  flexDirection: 'column',
+export const CART_TABLE = {
+  fontFamily:
+    'Human BBY Web,Arial,Helvetica,Nimbus Sans L,Liberation Sans,FreeSans,sans-serif',
+  borderCollapse: 'collapse',
+  width: '100%',
 };
