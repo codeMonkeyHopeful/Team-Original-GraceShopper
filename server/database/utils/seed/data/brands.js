a5b95012f62ab313c88b16601defdf70d0ab5ff8
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-07 15:16:18 -0400

    test

--- server/database/utils/seed/data/brands.js
+++
@@ -1,9 +0,0 @@
-const brands = [
-  {
-    name: 'Asus',
-    brand_image_url:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjyVRVHxB1Z56bIxEyugN1MLVQ-85fwjRBeFcwuEBAVybIdNhV',
-  },
-];
-
-module.exports = brands;
