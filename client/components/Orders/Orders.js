7f97eb7e86eb5397040bc1f208b27b8b3bb5ad57
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-08 17:20:14 -0400

    order table working

--- client/components/Orders/Orders.js
+++ client/components/Orders/Orders.js
@@ -1,7 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { isTerminatorless } from '@babel/types';
 
 const Orders = () => {
-  return <div>This is working</div>;
+  const [orderItems, setOrderItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/orders')
+      .then(result => {
+        console.log('axios');
+        setOrderItems(() => [...result.data]);
+      })
+      .catch(e => console.error(e));
+  }, []);
+
+  console.log('this is a test: ', orderItems);
+
+  return (
+    <div>
+      <h2>Your Previous Orders</h2>
+      <table>
+        <th>Product ID</th>
+        <th>Quantity</th>
+        <th>Price</th>
+        <th>Purchase Date</th>
+        {orderItems.map(item => {
+          return (
+            <tr>
+              <td>{item.productId}</td>
+              <td>{item.qty}</td>
+              <td>{item.price}</td>
+              <td>{item.timeStamp}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
 };
 
 export default Orders;
