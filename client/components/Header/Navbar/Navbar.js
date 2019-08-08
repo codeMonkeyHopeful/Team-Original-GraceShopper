44a84d6f9b57d530ce7fddcc3b5a894033d216e7
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-08 16:37:25 -0400

    remove brand button, seeding fake purchase

--- client/components/Header/Navbar/Navbar.js
+++ client/components/Header/Navbar/Navbar.js
@@ -15,14 +15,20 @@ const Navbar = () => {
   const navbarButtons = ['account', 'orders', 'cart'];
 
   return (
-    <div id="nav-links-container" style={NAV_LINKS_CONTAINER}>
-      <div id="nav-products-container">
+    <div id='nav-links-container' style={NAV_LINKS_CONTAINER}>
+      <div id='nav-products-container'>
         {/* modals */}
-        <NavLink to="/products" style={PRODUCT_MODAL}>
+        <NavLink to='/products' style={PRODUCT_MODAL}>
           <button>Products</button>
         </NavLink>
+<<<<<<< HEAD
+=======
+        {/* <NavLink to="/brands" style={PRODUCT_MODAL}>
+          <button>Brands</button>
+        </NavLink> */}
+>>>>>>> remove brand button, seeding fake purchase
       </div>
-      <div id="nav-account-links-container" style={NAV_ACCOUNT_LINKS_CONTAINER}>
+      <div id='nav-account-links-container' style={NAV_ACCOUNT_LINKS_CONTAINER}>
         {/* login status should come from redux */}
 
         <LoginLink />
--- server/database/utils/seed/data/orders.js
+++ server/database/utils/seed/data/orders.js
@@ -1,3 +1,10 @@
-const orders = [{}];
+const orders = [
+  {
+    qty: 5,
+    price: 599.99,
+    purchased: true,
+    userId: 1,
+  },
+];
 
 module.exports = orders;
