b46d90b595478b2360e5d10ad1e87e299c944395
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-07 20:12:42 -0400

    working orders api route

--- server/api_routes/orders.js
+++ server/api_routes/orders.js
@@ -1,20 +1,17 @@
-const Sequelize = require('sequelize');
 const router = require('express').Router();
 const { Cart } = require('../database/index.js');
-const { db } = require('./../database/index');
 
 router.get('/', (req, res, next) => {
-  console.log('user:', req.session.user.user_id);
-  if (req.session.user.user_id) {
+  if (req.session.user && req.session.user.user_id) {
     return Cart.findAll({
       where: { purchased: true, userId: req.session.user.user_id },
     })
       .then(purchases => {
         res.json(purchases);
       })
-      .catch(next());
+      .catch(next);
   } else {
-    console.log('not signed in');
+    res.sendStatus(400);
   }
 });
 
